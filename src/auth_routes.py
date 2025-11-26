214e7a370be0836b7d0ba73c8c84bc117e1a5d52
isabelaalmeida <isabelapantaleao24@gmail.com>
2025-11-26 14:21:45 -0300

    alterei o texto da descrição de autenticar

--- src/auth_routes.py
+++ src/auth_routes.py
@@ -6,6 +6,33 @@ auth = APIRouter(prefix="/autenticacao", tags=["auth"])
 @auth.get("/")
 async def autenticar():
     """
-    Essa é a rota padrão de autenticação do sistema
+    Essa é a rota padrão de autenticação do sistema. Nela, o usuário vai selecionar se é professor, coordenador ou administrador
     """
     return{"mensagem": "Você acessou a rota padrão de autenticação", "autenticado": False}
+
+@auth.get("/autenticar/administrador")
+async def login_administrador(dados: dict):
+    """
+    Essa é a rota para acessar a tela de login de Administrador
+    """
+    matricula = dados["matricula"]
+    senha = dados["senha"]
+    return {"mensagem": "Login realizado"}
+
+@auth.get("/autenticar/coordenador")
+async def login_coordenador(dados: dict):
+    """
+    Essa é a rota para acessar a tela de login de Coordenador
+    """
+    rmatricula = dados["matricula"]
+    senha = dados["senha"]
+    return {"mensagem": "Login realizado"}
+
+@auth.get("/autenticar/professor")
+async def login_professor(dados: dict):
+    """
+    Essa é a rota para acessar a tela de login de Professor
+    """
+    rmatricula = dados["matricula"]
+    senha = dados["senha"]
+    return {"mensagem": "Login realizado"}
