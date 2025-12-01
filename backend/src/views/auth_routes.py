828fd3633ed8250d7141fc39ab3c882a4c51591b
isabelaalmeida <isabelapantaleao24@gmail.com>
2025-12-01 00:31:31 -0300

    Concluí os endpoints já com a conexão com o banco de dados funcionando

--- backend/src/views/auth_routes.py
+++ backend/src/views/auth_routes.py
@@ -1,5 +1,10 @@
-from sys import prefix
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..database import get_db
+from ..repositories.administrador import AdministradorRepo
+from ..repositories.coordenador import CoordenadorRepos
+from ..repositories.professor import ProfessorRepos
+from ..repositories.escola import EscolaRepos
 
 auth = APIRouter(prefix="/autenticacao", tags=["auth"])
 
@@ -8,30 +13,63 @@ async def autenticar():
     """
     Essa é a rota padrão de autenticação do sistema. Nela, o usuário vai selecionar se é professor, coordenador ou administrador
     """
-    return{"mensagem": "Você acessou a rota padrão de autenticação", "autenticado": False}
+    return{"mensagem": "Você acessou a rota padrão de autenticação"}
 
 @auth.post("/autenticar/administrador")
-async def login_administrador(dados: dict):
+async def login_administrador(matricula: int, db: Session = Depends(get_db)):
     """
-    Essa é a rota para acessar a tela de login de Administrador
+    Essa é a rota para a matrícula de Administrador no banco de dados
     """
-    matricula = dados["matricula"]
-    return {"mensagem": "Cadastro encontrado"}
+    repo = AdministradorRepo()
+    result = repo.buscar_por_id(db, matricula)
+    print("Resultado -> ", result)
+    if (result is None):
+        return {"Cadastro não encontrado"}
+    dados = {
+        "matricula": result.matricula,
+        "nome": result.nome,
+        "email": result.email,
+        "bairro": result.bairro,
+        "uf": result.uf
+    }
+    return {"Cadastro" : dados}
 
 @auth.post("/autenticar/coordenador")
-async def login_coordenador(dados: dict):
+async def login_coordenador(matricula: int, db: Session = Depends(get_db)):
     """
-    Essa é a rota para acessar a tela de login de Coordenador
+    Essa é a rota para a matrícula de Coordenador no banco de dados
     """
-    rmatricula = dados["matricula"]
-    senha = dados["senha"]
-    return {"mensagem": "Cadastro encontrado"}
+    repo = CoordenadorRepos()
+    result = repo.buscar_por_id(db, matricula)
+    print("Resultado -> ", result)
+    if (result is None):
+        return {"Cadastro não encontrado"}
+    dados = {
+        "matricula": result.matricula,
+        "nome": result.nome,
+        "situacao": result.situacao,
+        "id_escola": result.id_escola
+    }
+    if (dados.get("situacao") == "inativo"):
+        return {"Usuário inativo"}
+    return {"Cadastro" : dados}
 
 @auth.post("/autenticar/professor")
-async def login_professor(dados: dict):
+async def login_professor(matricula: int, db: Session = Depends(get_db)):
     """
-    Essa é a rota para acessar a tela de login de Professor
+    Essa é a rota para a matrícula de Professor no banco de dados
     """
-    rmatricula = dados["matricula"]
-    senha = dados["senha"]
-    return {"mensagem": "Cadastro encontrado"}
+    repo = ProfessorRepos()
+    result = repo.buscar_por_id(db, matricula)
+    print("Resultado -> ", result)
+    if (result is None):
+        return {"Cadastro não encontrado"}
+    dados = {
+        "matricula": result.matricula,
+        "nome": result.nome,
+        "situacao": result.situacao,
+        "id_escola": result.id_escola
+    }
+    if (dados.get("situacao") == "inativo"):
+        return {"Usuário inativo"}
+    return {"Cadastro" : dados}
