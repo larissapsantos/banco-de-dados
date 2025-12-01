7112011db8ce3c45c39a0f216874845693f2212d
isabelaalmeida <isabelapantaleao24@gmail.com>
2025-11-30 21:26:37 -0300

    Movi or arquivos com as rotas para a pasta views

--- backend/src/views/auth_routes.py
+++ backend/src/views/auth_routes.py
@@ -3,36 +3,35 @@ from fastapi import APIRouter
 
 auth = APIRouter(prefix="/autenticacao", tags=["auth"])
 
-@auth.get("/")
+@auth.post("/")
 async def autenticar():
     """
     Essa é a rota padrão de autenticação do sistema. Nela, o usuário vai selecionar se é professor, coordenador ou administrador
     """
     return{"mensagem": "Você acessou a rota padrão de autenticação", "autenticado": False}
 
-@auth.get("/autenticar/administrador")
+@auth.post("/autenticar/administrador")
 async def login_administrador(dados: dict):
     """
     Essa é a rota para acessar a tela de login de Administrador
     """
     matricula = dados["matricula"]
-    senha = dados["senha"]
-    return {"mensagem": "Login realizado"}
+    return {"mensagem": "Cadastro encontrado"}
 
-@auth.get("/autenticar/coordenador")
+@auth.post("/autenticar/coordenador")
 async def login_coordenador(dados: dict):
     """
     Essa é a rota para acessar a tela de login de Coordenador
     """
     rmatricula = dados["matricula"]
     senha = dados["senha"]
-    return {"mensagem": "Login realizado"}
+    return {"mensagem": "Cadastro encontrado"}
 
-@auth.get("/autenticar/professor")
+@auth.post("/autenticar/professor")
 async def login_professor(dados: dict):
     """
     Essa é a rota para acessar a tela de login de Professor
     """
     rmatricula = dados["matricula"]
     senha = dados["senha"]
-    return {"mensagem": "Login realizado"}
+    return {"mensagem": "Cadastro encontrado"}
