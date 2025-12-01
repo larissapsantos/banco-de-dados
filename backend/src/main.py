7f8c027f44cab5633d9ecc6dddf4aee1f570bae9
isabelaalmeida <isabelapantaleao24@gmail.com>
2025-12-01 00:29:20 -0300

    Defini a rota principal da API

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,18 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-from src.auth_routes import auth
-from src.request_routes import request
+from src.views.auth_routes import auth
+from src.views.request_routes import request
 
 app.include_router(auth)
 app.include_router(request)
 
+@app.get("/")
+async def root():
+    return {"message": "Sistema funcionando!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
 # Para rodar o código, executar no terminal: uvicorn src.main:app --reload
-
-# endpoints: /tela-inicial ou /tela-login
-
-# REST APIS
-# Get - leitura/pegar
-# Post - enviar/criar
-# Put - edição
-# Delete - deletar
